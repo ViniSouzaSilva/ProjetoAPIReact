39888ecc087a563d579067e107df40a24e7455ea
vinicius souza <vinicius291996@hotmail.com>
2025-01-27 17:52:15 -0300

    deploy

--- React/reacttutorial/src/Pages/IA/IA.js
+++ React/reacttutorial/src/Pages/IA/IA.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
+import { Input,Card, Text,Button, VStack, TagGroup, Tag } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 function IA() {
   const [response, setResponse] = useState(null);
   const handleInputChange = (event) => {
@@ -14,7 +15,7 @@ function IA() {
       const { GoogleGenerativeAI } = require("@google/generative-ai");
       const genAI = new GoogleGenerativeAI("AIzaSyCRmgAvyfuSSA334BjUtpcl_jcj7PCDElQ"); 
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-      const prompt = promptDigitado;
+      const prompt = promptDigitado + "seja bem amigável e descontraído na resposta e limite a resposta em 900 caracteres";
       const result = await model.generateContent(prompt);
       setResponse(result.response.text()); 
     } catch (error) {
@@ -31,15 +32,40 @@ function IA() {
 
   return (
     <div>
-      {response ? <p>{response}</p> : <p>Carregando...</p>} 
+      
 
-      <input 
+      
+     
+
+
+      <Card width={500} shaded direction="row">
+      <img
+        src="https://images.unsplash.com/photo-1576606539605-b2a44fa58467?q=80&w=1974&auto=format&fit=crop"
+        alt="Shadow"
+        width={200}
+        style={{ objectFit: 'cover' }}
+      />
+      <VStack spacing={2}>
+        <Card.Header as="h5">Cream</Card.Header>
+        <Card.Body>
+        {response ? <p>{response}</p> : <p>Carregando...</p>} 
+        </Card.Body>
+        <Card.Footer>
+          <TagGroup>
+            <Tag size="sm"></Tag>
+            <Tag size="sm"></Tag>
+            <Tag size="sm"></Tag>
+          </TagGroup>
+        </Card.Footer>
+      </VStack>
+    </Card>
+    <input 
         type="text" 
-        value={promptDigitado} 
+        value={promptDigitado } 
         onChange={handleInputChange} 
         placeholder="Digite algo"
       />
-      <button onClick={fetchData}>Buscar</button>
+    <Button onClick={fetchData}>Buscar</Button>
     </div>
   );
 }
